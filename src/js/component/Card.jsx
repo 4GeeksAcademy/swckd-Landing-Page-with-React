3f818ced209fabebeab4a5e6c921051e385164a2
Alejandro <aleckbeu@gmail.com>
2024-04-01 18:22:06 +0000

    Alejandro

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
-
-
-const Card = ({ datosCartas }) => {
+const Card = ({ props }) => {
     return (
 
-        datosCartas.map(() => {
-            <div className="col">
-                <div className="card px-0" style={{ width: "100%" }}>
-                    <img src="" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+        props.map((prop, index) => {
+
+            return (
+                <div className="col" key={index}>
+                    <div className="card px-0" style={{ width: "100%" }}>
+                        <img src={prop.image_path} className="card-img-top" style={{ height: "100px" }} />
+                        <div className="card-body">
+                            <h5 className="card-title">{prop.title}</h5>
+                            <p className="card-text">{prop.description}</p>
+                            <a href="#" className="btn btn-primary">{prop.button_text}</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )
         })
 
     );
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -15,35 +15,35 @@ const Home = () => {
 			"id": 1,
 			"title": "Imagen 1",
 			"description": "Esta es la descripción de la imagen 1.",
-			"image_path": "https://www.istockphoto.com/photo/female-detective-looking-attentively-at-investigation-board-back-view-gm1457378613-492181037?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fprueba%2F&utm_term=prueba",
+			"image_path": "https://media.es.wired.com/photos/6501e7429fa9000811a95fe8/16:9/w_2560%2Cc_limit/Adobe%2520Firefly.jpeg",
 			"button_text": "Ver más"
 		},
 		{
 			"id": 2,
 			"title": "Imagen 2",
 			"description": "Esta es la descripción de la imagen 2.",
-			"image_path": "https://www.istockphoto.com/photo/female-detective-looking-attentively-at-investigation-board-back-view-gm1457378613-492181037?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fprueba%2F&utm_term=prueba",
+			"image_path": "https://media.es.wired.com/photos/650b2a2e72d73ca3bd5ef0cc/16:9/w_2560%2Cc_limit/Business-OpenAI-Dall-E-3-heart.jpg",
 			"button_text": "Detalles"
 		},
 		{
 			"id": 3,
 			"title": "Imagen 3",
 			"description": "Esta es la descripción de la imagen 3.",
-			"image_path": "https://live.staticflickr.com/65535/50950143706_b7a31e0a54_b.jpg",
+			"image_path": "https://static6.depositphotos.com/1100968/645/i/950/depositphotos_6455974-stock-photo-pathway-in-the-autumn-forest.jpg",
 			"button_text": "Explorar"
 		},
 		{
 			"id": 4,
 			"title": "Imagen 4",
 			"description": "Esta es la descripción de la imagen 4.",
-			"image_path": "https://www.istockphoto.com/photo/female-detective-looking-attentively-at-investigation-board-back-view-gm1457378613-492181037?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fprueba%2F&utm_term=prueba",
+			"image_path": "https://media.istockphoto.com/id/1193198754/es/foto/atardecer.webp?s=2048x2048&w=is&k=20&c=U0odjL_RKFjMlxWnZ92b85-zuLPh7a0NC8TUqIws4uw=",
 			"button_text": "Leer más"
 		},
 		{
 			"id": 5,
 			"title": "Imagen 5",
 			"description": "Esta es la descripción de la imagen 5.",
-			"image_path": "https://www.istockphoto.com/photo/female-detective-looking-attentively-at-investigation-board-back-view-gm1457378613-492181037?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fprueba%2F&utm_term=prueba",
+			"image_path": "https://media.istockphoto.com/id/817147678/es/foto/libro-de-papel-de-forma-de-coraz%C3%B3n-en-la-playa.jpg?s=612x612&w=0&k=20&c=SzK0a9UagX2E78ej0FNKbVZfWgY4SEVeVv_l6STyv9k=",
 			"button_text": "Descargar"
 		}
 	];
@@ -52,14 +52,10 @@ const Home = () => {
 	return (
 		<>
 			<Navbar />
-
 			<div className="container">
 				<Jumbotron />
-
 				<div className="row justify-content-center gy-1">
-
-					<Card data={datosCartas} />
-
+					<Card props={datosCartas} />
 				</div>
 			</div >
 		</>
